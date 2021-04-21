ab8e30d96d539d9baa5edff13677a57d5886434f
CYX <530911376@qq.com>
2021-04-22 00:46:37 +0800

    2021年4月22日 00:46:36

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FruitType
+{ 
+    LanMei = 1,
+    SangShen = 2,
+    JuZi = 3,
+    ChengZi = 4,
+    MiHouTao = 5,
+    XiHongShi = 6,
+    TaoZi = 7,
+    BoLuo = 8,
+    YeZi = 9,
+    XiGua = 10,
+    DaXiGua = 11
+}
+public class Fruit : MonoBehaviour
+{
+    public FruitType fruitType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
